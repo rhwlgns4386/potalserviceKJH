9f8bddac2db4c3d2b47cf7addeb7acdc90b1fcec
rhwlgns4386 <30650239+rhwlgns4386@users.noreply.github.com>
2021-06-09 23:18:21 +0900

    asf

--- src/main/java/kr/ac/jejunu/WebConfig.java
+++ src/main/java/kr/ac/jejunu/WebConfig.java
@@ -1,10 +1,14 @@
 package kr.ac.jejunu;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+import org.springframework.web.servlet.view.xml.MappingJackson2XmlView;
+
+import java.util.List;
+
 
 @Configuration
 @EnableWebMvc
@@ -12,11 +16,23 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new UserInterceptor());
-
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler()
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("/WEB-INF/static");
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.enableContentNegotiation(new MappingJackson2JsonView()
+                , new MappingJackson2XmlView());
+        registry.jsp().prefix("/WEB-INF/views/").suffix(".jsp");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter());
     }
 }
